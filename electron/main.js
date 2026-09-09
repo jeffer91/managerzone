bcb6054205635889c8119785d9278369661454b8
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-08 22:29:58 -0500

    Agregar proceso principal de Electron

---
+++ electron/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1440,
+    height: 920,
+    minWidth: 1120,
+    minHeight: 720,
+    backgroundColor: '#07110c',
+    title: 'MZ Tactical Lab',
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, '..', 'src', 'index.html'));
+}
+
+app.whenReady().then(() => {
+  Menu.setApplicationMenu(null);
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
